188b9a2cf9b328f6070d08cefac0127901c80ea0
BugHunter-X <askzenai@gmail.com>
2025-02-20 23:54:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,10 @@
+```javascript
+// Solution might involve more specific selectors or CSS overrides
+<div class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" style="z-index: 10;">
+  Hover over me!
+</div>
+//or
+<div class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+  <span class="hover:bg-red-700">Hover over me!</span>
+</div>
+```
